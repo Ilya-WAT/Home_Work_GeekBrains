622771f55b852137fd65e9995bb4a91172898056
WAT <sugak.ilia@yandex.ru>
2024-10-26 20:21:25 +0300

    Создание программы натуральных чисел в промежутке M, N с использованием цикла

---
+++ Recursion/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите значение M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+void get_numbers_between(int first,int second)
+{
+    for (int i = first;i<=second;i++)
+    {
+        Console.WriteLine(i);
+    }
+}
+get_numbers_between(M,N);
+
